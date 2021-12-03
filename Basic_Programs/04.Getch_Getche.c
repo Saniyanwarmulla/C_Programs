cb897b1ac519c06c16f4b58860dabd1ea46901eb
Saniyanwarmulla <mullasaniya056@gmail.com>
2021-12-03 15:18:38 +0530

    Basic Program

---
+++ Basic_Programs/04.Getch_Getche.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+        char Name = getchar();
+
+
+         putch('\n');
+         putch('\n');
+         putch('ABC');
+         putch('\n');
+
+         getche();
+
+         putchar('\t');
+         putchar('name');
+         putchar('\n');
+         putchar('W');
+
+         getche();
+
+
+         getch();
+
+         return 0;
+
+
+}
