4f47afdeeaafec3a9b878f50d2b617b46cc69634
Saniyanwarmulla <mullasaniya056@gmail.com>
2021-12-04 09:05:17 +0530

    Basic Program

---
+++ Basic_Programs/06.Getch_Getchar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+            char  ch = '\0';
+
+            ch = getchar();
+
+            printf("\n Welcome To Fork Infosystems!!!\n ");
+
+            printf("\n Value of Given Character = %c",ch);
+
+             getche();
+             _getch();
+
+            return 0;
+}
+
