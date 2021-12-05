663c87dc1f1abb298fc8019747340243910ef50d
Saniyanwarmulla <mullasaniya056@gmail.com>
2021-12-05 13:45:34 +0530

    Basic Program

---
+++ Basic_Programs/07.Printf_Non_Formatted_Output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+      printf("\n \t\t Student Database \n");
+
+      printf("\n %d %s %d %d %d %d %f", 120,"Amitabh",6,94,64,133,56.222);
+      printf("\n %d %s %d %d %d %d %f", 21,"Ajay",89,45,35,400,66.321);
+      printf("\n %d %s %d %d %d %d %f", 45,"Amruta",85,84,86,87.888);
+
+      _getch();
+      return 0;
+}
