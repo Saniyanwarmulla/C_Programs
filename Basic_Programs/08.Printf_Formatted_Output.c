c191005c467e84b12cf6d2c1aca284cf61f9159a
Saniyanwarmulla <mullasaniya056@gmail.com>
2021-12-06 07:52:57 +0530

    Basic Program

--- Basic_Programs/08.Printf_Formatted_Output.c
+++ Basic_Programs/08.Printf_Formatted_Output.c
@@ -2,8 +2,9 @@
 #include<conio.h>
 int main()
 {
-    printf("\n %4d %-10s %-10s %3d %3d %3d %3d %6.2f",255, "Amit", "Poll",100,100,100,30,80.30);
-    printf("\n %4d %-10s %-10s %3d %3d %3d %3d %6.2f",23, "Akshay","patil",95,81,74,55,77.23);
+
+    printf("\n %4d %-10s %-10s %3d %3d %3d %3d %6.2f",255, "Amit" , "Poll" , 100,100,100,30,80.30);
+    printf("\n %4d %-10s %-10s %3d %3d %3d %3d %6.2f",23, "Akshay","patil" , 95,81,74,55,77.23);
 
     _getch();
     return 0;
