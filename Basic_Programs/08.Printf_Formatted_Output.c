d27e8a63b0ef4bafb3c36e1c11e7b382064abe59
Saniyanwarmulla <mullasaniya056@gmail.com>
2021-12-06 07:54:39 +0530

    Basic Program

--- Basic_Programs/08.Printf_Formatted_Output.c
+++ Basic_Programs/08.Printf_Formatted_Output.c
@@ -4,8 +4,8 @@ int main()
 {
 
     printf("\n %4d %-10s %-10s %3d %3d %3d %3d %6.2f",255, "Amit" , "Poll" , 100,100,100,30,80.30);
-    printf("\n %4d %-10s %-10s %3d %3d %3d %3d %6.2f",23, "Akshay","patil" , 95,81,74,55,77.23);
+    printf("\n %4d %-10s %-10s %3d %3d %3d %3d %6.2f",23, "Akshay","Patil" , 95,81,74,55,77.23);
 
-    _getch();
+    getch();
     return 0;
 }
