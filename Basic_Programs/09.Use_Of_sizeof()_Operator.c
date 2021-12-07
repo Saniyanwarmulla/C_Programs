43d15b46245d2de4eff0e9b57161494e4e6ee666
Saniyanwarmulla <mullasaniya056@gmail.com>
2021-12-07 12:42:38 +0530

    Basic Program

---
+++ Basic_Programs/09.Use_Of_sizeof()_Operator.c
@@ -0,0 +1,28 @@
+// Use of sizeof() operators
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+      int Num = 21;
+      char ch = 'Q';
+
+
+      printf("\n Size for Integer in this environment = %d.", sizeof(Num));
+      printf("\n Size for Character in this environment = %d.", sizeof(char));
+
+
+      printf("\n--------------------****------------------------\n");
+
+
+      printf("\n Size for Integer in this environment = %d.", sizeof(int));
+      printf("\n Size for Character in this environment = %d.",sizeof(char));
+      printf("\n Size for Short Integer in this environment = %d.",sizeof(short int));
+      printf("\n Size for Float in this environment = %d.",sizeof(float));
+      printf("\n Size for Double in this environment = %d.", sizeof(double));
+
+      _getch();
+      return 0;
+
+}
