9fd7ac0fe15e4fbfe29ed79a8ffdef9fbe67978c
Saniyanwarmulla <mullasaniya056@gmail.com>
2021-12-10 13:28:53 +0530

    Basic Program

---
+++ Basic_Programs/11.Swapping_Of_Two_Numbers_Without_Funtion_Approach.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+          int Num1 = 0,Num2 = 0;
+
+          printf("\n Enter Two Numbers for Swapping = ");
+          scanf("%d%d",&Num1,&Num2);
+
+          printf("\n Before Swapping First Number = %d.",Num1);
+          printf("\n Before Swapping Second Number = %d.",Num2);
+
+          Num1 = Num1+Num2;
+          Num2 = Num1-Num2;
+          Num1 = Num1-Num2;
+
+          printf("\n After Swapping First Number = %d.",Num1);
+          printf("\n After Swapping Second Number = %d.",Num2);
+
+          printf("\n\n Thanks....!!!");
+
+          _getch();
+          return 0;
+
+
+}
