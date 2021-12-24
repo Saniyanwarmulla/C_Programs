cad52303e8d51b360f1d22fef24335f26833e6d8
Saniyanwarmulla <mullasaniya056@gmail.com>
2021-12-24 09:59:45 +0530

    Basic Program

---
+++ Basic_Programs/13.Program_To_Display_ASCII_Value_To_Character_Conversion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int No = 0;
+
+         printf("\n ASCII To Character Conversion:");
+
+         printf("\n Enter ASCII Value To Get Its CorrespondIng Character= ");
+         scanf("%d",&No);
+
+         printf("\n\n Character for Given ASCII value %d is = |%c|.",No,No);
+
+         printf("\n\n Thanks for using our service...");
+         _getch();
+         return 0;
+
+}
