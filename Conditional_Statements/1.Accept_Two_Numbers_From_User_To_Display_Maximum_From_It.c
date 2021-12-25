9e1e72f0adf0881518d3ab911ba771d8793552d4
Saniyanwarmulla <mullasaniya056@gmail.com>
2021-12-25 12:33:38 +0530

    Basic Program

---
+++ Conditional_Statements/1.Accept_Two_Numbers_From_User_To_Display_Maximum_From_It.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+    {
+             int Num1 = 0,Num2 = 0;
+
+             printf("\n Enter Two Integer Numbers: ");
+             scanf("%d%d",&Num1,&Num2);
+
+             if(Num1 == Num2)
+             {
+                   printf("\n Both Given Numbers are Equal.");
+             }
+             else if(Num1 > Num2)
+             {
+                   printf("\n Number %d is Maximum.",Num1);
+             }
+             else
+             {
+                   printf("\n Number %d is Maximum.",Num2);
+             }
+
+             printf("\n Thanks..!!");
+             _getch();
+             return 0;
+    }
