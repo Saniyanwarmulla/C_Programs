359a6ee4b172ece92b9db09c2e170eefccbb571f
Saniyanwarmulla <mullasaniya056@gmail.com>
2022-02-21 15:49:39 +0530

    Conditional Program

---
+++ Conditional_Statements/2.Accept_Two_Number_From_User_To_Display_Maximum_From_It (Using Ternary).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+         int No1 = 0,No2 = 0;
+         Grade:
+
+         printf("\n Enter Two Integer Numbers for Display Maximum: ");
+         scanf("%d%d",&No1,&No2);
+
+         if(No1 == No2)
+         {
+              printf("\n Both Given Numbers are Equal.\n Please Enter Different Number to compare.\n");
+              goto Grade;
+         }
+
+         (No1 > No2)? printf("\n Number %d is Maximum.",No1) : printf("\n Number %d is Maximum.",No2);
+
+         _getch();
+         return 0;
+}
